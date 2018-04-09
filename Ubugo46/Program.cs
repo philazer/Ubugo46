492277c9d7ac28e1aecb8d076fc8bb69d7bb5fac
philazer <philippe.francis@hotmail.com>
2018-04-09 17:16:53 +0200

    c1

--- Ubugo46/Program.cs
+++ Ubugo46/Program.cs
@@ -15,6 +15,7 @@ namespace Ubugo46
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+            //*************
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
